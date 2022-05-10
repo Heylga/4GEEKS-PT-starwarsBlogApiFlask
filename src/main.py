10d018d3e3a31aa89c84336cc90de5bfe86a87a3
Heylga <oy.golovina@gmail.com>
2022-05-10 13:57:53 +0000

    People section created and API GET method launched, status  200 OK

--- src/main.py
+++ src/main.py
@@ -39,6 +39,41 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+@app.route('/people', methods=['GET'])
+def get_people():
+    name = request.json.get("name", None)
+    gender = request.json.get("gender", None)
+    hair_color = request.json.get("hair_color", None)
+    eye_color = request.json.get("eye_color", None)
+
+    if name is None:
+        return jsonify({"msg": "Name is required"}), 400
+    if gender is None:
+        return jsonify({"msg": "Gender is required"}), 400
+    if hair_color is None:
+        return jsonify({"msg": "Hair Color is required"}), 400
+    if eye_color is None:
+        return jsonify({"msg": "Eye color is required"}), 400
+
+    people = people.query.filter_by(name=name, gender=gender,hair_color=hair_color,eye_color=eye_color).first()
+
+    if people:
+        # the user was not found on the database
+        return jsonify({"msg": "character was already registered"}), 401
+    else:
+        # crea usuario nuevo
+        # crea registro nuevo en BBDD de
+        people = people(name=name, gender=gender, hair_color=hair_color,eye_color=eye_color)
+        db.session.add(people)
+        db.session.commit()
+
+
+    response_body = {
+        "msg": "Hello, this is your GET /people response "
+    }
+
+    return jsonify(response_body), 200
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
